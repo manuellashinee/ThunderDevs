5afc6691658bb73e95d84f4d80fbcdcb7604d333
manuellashinee <manujesus.vasconcelos@gmail.com>
2022-09-13 09:40:52 -0300

    começo do login, bem cru msm

--- site/src/App.js
+++ site/src/App.js
@@ -2,11 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
+    <section className='container'>
+      <div>
+        <img  src='../images/logo.svg'/>
+      </div>
+
+      <div className='quadro'>
+        <div className='faixa'><h1>LOGIN</h1></div>
+
+        <div className='squad1'>
+          <div> <p>EMAIL:</p></div>
+          <div><input/></div>
+        </div>
+        
+      </div>
+    </section>
   );
 }
 
