6446f89eb1f964aff0a79576c6f72318599e7f42
manuellashinee <manujesus.vasconcelos@gmail.com>
2022-09-14 09:43:23 -0300

    loginnnn

--- site/src/App.js
+++ site/src/App.js
@@ -3,18 +3,29 @@ import './App.css';
 function App() {
   return (
     <section className='container'>
-      <div>
-        <img  src='../images/logo.svg'/>
+      <div className='la'>
+        <img className='logo' src='../images/logo.svg'/>
       </div>
-
+      <div className='a'>
       <div className='quadro'>
-        <div className='faixa'><h1>LOGIN</h1></div>
+        <div className='faixa'><h1>LOGIN</h1> <img src='../images/cortapra18.svg'/></div>
 
+       <div className='she'>
+    
+       <div className='harry'>
         <div className='squad1'>
-          <div> <p>EMAIL:</p></div>
-          <div><input/></div>
+          <div> <p>E-MAIL:</p></div>
+          <div><input className='can'/></div>
         </div>
-        
+        <div className='squad2'>
+          <div> <p>SENHA:</p></div>
+          <div><input className='can'/></div>
+        </div>
+        </div>
+        <div className='descricao'><p>FAÇA SEU LOGIN PARA TER ACESSO AO NOSSO CATÁLOGO IMPERDÍVEL, NOSSO PLANO CINEMA, BONÛS E OFERTAS UNICOS!</p></div>
+       </div>
+
+      </div>
       </div>
     </section>
   );
