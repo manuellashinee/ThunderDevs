88e775b7a9398a2c1fee0cc0aabcf66e771db7df
manuellashinee <manujesus.vasconcelos@gmail.com>
2022-09-19 09:35:18 -0300

    pulllll

--- api/src/controller/filmeController.js
+++ api/src/controller/filmeController.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 const server = Router();
-import { CadastrarFilme } from "../repository/usuarioRepository";
+
+import multer from "multer" ;
+const upload = multer({dest: 'storage/fotosfilmes'}) 
 
 
-erver.post('/adm/CadastrarFilme' , async (req, resp) => {
-    
-}
+
