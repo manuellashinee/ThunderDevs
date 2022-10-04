342dd33e82c4df26557f50d02e4974fd105a56a5
juliana <julianafatima5099@gmail.com>
2022-10-04 09:31:23 -0300

    vai toma no cu caje

--- site/src/pages/adm/homeadm/index.js
+++ site/src/pages/adm/homeadm/index.js
@@ -4,11 +4,17 @@ import '../../../common/common.scss'
 export default function HomeAdm() {
     return(
         <section className='home-adm'>
-            <div className='cabecalho-homeadm'>
+            <div className='parte-cima'>
+                <div className='cabecalho-homeadm'>
                 <img className='logo' src='../images/logo.svg'/>
-                <div>
+                <div className='icon-texto'>
                     <img className='icon-persona' src='../images/popcorn.png'/>
                     <p className='texto-catalogo'>juliana</p>
+                </div>
+                </div>
+                <div className='tt-adm'>
+                    <h1 className='titulo-adm'>BEM VINDO, <span className='cor-texto'>ADMISTRADOR(A)!</span></h1>
+                    <p className='sub-titu'>DESEJAMOS AS BOAS VINDAS AO SISTEMA <span className='sub-titu-s'>POPCORN TIME</span></p>
                 </div>
             </div>
         </section>
