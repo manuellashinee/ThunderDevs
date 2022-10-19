021a2b9efbebd5c8c251348858fb12bc54e774ee
juliana <julianafatima5099@gmail.com>
2022-10-19 14:58:15 -0300

    ta feito acho?

--- site/src/pages/user/assentos/index.js
+++ site/src/pages/user/assentos/index.js
@@ -11,6 +11,182 @@ export default function Assento(){
                 <Link to='/'><img className='flecha-c' src='../images/flecha.svg' /></Link>
                 <p className='botao-continuar'>CONTINUAR</p>
             </div>
+
+            <div className='assentos-inicio'>
+                <div className='assento-row'>
+                 <p className='letra-assento1'>A</p>
+                    <div className='assento-cima'>
+                        <div className='assento-cima2'>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                         </div>
+                        </div>
+                    <p className='letra-assento'>A</p>
+                </div>
+                
+                <div className='assento-row'>
+                 <p className='letra-assento'>B</p>
+                    <div className='assento-baixo'>
+                        <div className='assento-cima2'>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p2'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                         </div>
+                        </div>
+                    <p className='letra-assento'>B</p>
+                </div>
+
+                
+                <div className='assento-row'>
+                 <p className='letra-assento'>C</p>
+                    <div className='assento-baixo'>
+                        <div className='assento-cima2'>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p2'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                         </div>
+                        </div>
+                    <p className='letra-assento'>C</p>
+                </div>
+
+                
+                <div className='assento-row'>
+                 <p className='letra-assento'>D</p>
+                    <div className='assento-baixo'>
+                        <div className='assento-cima2'>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p2'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                         </div>
+                        </div>
+                    <p className='letra-assento'>D</p>
+                </div>
+
+                
+                <div className='assento-row'>
+                 <p className='letra-assento'>E</p>
+                    <div className='assento-baixo'>
+                        <div className='assento-cima2'>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p2'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                         </div>
+                        </div>
+                    <p className='letra-assento'>E</p>
+                </div>
+
+                
+                <div className='assento-row'>
+                 <p className='letra-assento'>F</p>
+                    <div className='assento-baixo'>
+                        <div className='assento-cima2'>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p2'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                         </div>
+                        </div>
+                    <p className='letra-assento'>F</p>
+                </div>
+
+                
+                <div className='assento-row'>
+                 <p className='letra-assento'>G</p>
+                    <div className='assento-baixo'>
+                        <div className='assento-cima2'>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p2'>A</p>
+                            <p className='assento-p'>A</p>
+                            <p className='assento-p'>A</p>
+                         </div>
+                        </div>
+                    <p className='letra-assento'>G</p>
+                </div>
+
+                
+                <div className='numeros'>
+                    <p className='n'>1</p>
+                    <p className='n'>2</p>
+                    <p className='n'>3</p>
+                    <p className='n'>4</p>
+                    <p className='n'>5</p>
+                    <p className='n'>6</p>
+                    <p className='n'>7</p>
+                    <p className='n'>8</p>
+                    <p className='n'>9</p>
+                    <p className='n'>10</p>
+                    <p className='n'>11</p>
+                    <p className='n'>12</p>
+                    <p className='n'>13</p>
+                    <p className='n'>14</p>
+                    <p className='n'>15</p>
+                    
+                </div>
+            </div>
+
+            <div className='tela'>
+               <p className='tela2'>TELA</p> 
+            </div>
         </section>
     );
 }
