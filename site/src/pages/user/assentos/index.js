ea6d40dd259634ecdf8ccc0985cb0e423b9aded5
SevuloB <matheusservulo4@gmail.comB>
2022-10-19 09:44:27 -0300

    sla

--- site/src/pages/user/assentos/index.js
+++ site/src/pages/user/assentos/index.js
@@ -1,10 +1,16 @@
 import './index.scss'
 import '../../../common/common.scss'
+import { Link } from 'react-router-dom'
+import CabecalhoP from '../../components/cabecalho-sem-p';
 
 export default function Assento(){
     return(
-        <section>
-            <h1>ioooo</h1>
+        <section className='assento-page'>
+            <CabecalhoP/>
+            <div className='seta-botao'>
+                <Link to='/'><img className='flecha-c' src='../images/flecha.svg' /></Link>
+                <p className='botao-continuar'>CONTINUAR</p>
+            </div>
         </section>
     );
 }
