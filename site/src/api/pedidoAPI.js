512df495347b785c77c48d6a932268359183b6b1
juliana <julianafatima5099@gmail.com>
2022-10-30 17:22:44 -0300

    ?

--- site/src/api/pedidoAPI.js
+++ site/src/api/pedidoAPI.js
@@ -1,11 +1,11 @@
 import { api } from "./url.js";
 
 import axios from 'axios'
-const api = axios.create({
+const API = axios.create({
     baseURL: api
 })
 
 export async function salvarNovoPedido(idUsuario, novoPedido){
-    const r = await api.post('/api/pedidocombo/' + idUsuario, novoPedido);
+    const r = await API.post('/api/pedidocombo/' + idUsuario, novoPedido);
     return r.data;
 }
