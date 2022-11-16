9a6e063d6b6b3340ae995cd959425503d7aa0075
Matheus <matheusservulo4@gmail.com>
2022-11-16 16:09:29 -0300

    é isso caralho falta muito ainda , mas consegui um parte bem fdp

--- site/src/pages/components/Hora3/index.js
+++ site/src/pages/components/Hora3/index.js
@@ -1,19 +1,27 @@
 import './index.scss'
+import Storage from "local-storage"
 import '../../../common/common.scss'
+import { useState } from 'react'
 
 export default function Hora3(props){
+    
+    function marcadin(hora,sala) {
+        if(Storage("ingresso")[1]){
+            
+        console.log(Storage("ingresso")[1].horario)
+        console.log(hora)
 
-    function marcado() {
-        if (props.marcado === true)
+        if (Storage("ingresso")[1].horario === hora && Storage("ingresso")[1].sala === sala )
             return 'horario-marcado';
-        else 
-            return ''
+        else
+            return '';
+        }
     }
 
 
     return (
         <section className='data-inicial2'>
-            <p className={`data-p2 ${marcado()}`}>{props.item.HORARIO}</p>
+            <p className={`data-p2 ${marcadin(props.item.HORARIO,props.item.SALA)}`}>{props.item.HORARIO}</p>
         </section>
     );
 }
